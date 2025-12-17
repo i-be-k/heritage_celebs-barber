8fb6d36057ae6415430a39ef6adf3cf068ff965f
unknown <Opierah@Opsydev.(none)>
2025-12-17 18:37:53 +0100

    Completed Sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import { FaBars, FaXmark } from 'react-icons/fa6';
 
 const Header = () => {
+    
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+    
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    }
+
+    const navItems = [
+        { link: 'Home', path: 'home' },
+        { link: 'About', path: 'about' },
+        { link: 'Pricing', path: 'pricing' },
+        { link: 'Services', path: 'services' },
+        { link: 'Testimonial', path: 'testimonial' },
+        { link: 'Contact', path: 'contact' },
+    ]
+    
     return (
-        <div>Header</div>
+        <nav className='flex justify-between items-center gap-4 bg-purple-600 lg:px-10 px-4 py-6 sticky top-0 z-30 border-[8px] border-[#a39446]'>
+            <div id='logo'>
+                <h1 className='text-white font-bold text-5xl'>H<span className='italic text-yellow'>CB</span></h1>
+            </div>
+            <ul className='lg:flex justify-center items-center gap-6 hidden'>
+                {navItems.map(({ link, path}) => (
+                    <Link key={path} className='text-white uppercase font-semibold cursor-pointer p-3 rounded-lg hover:bg-yellow hover:text-black' to={path} spy={true} offset={-100} smooth={true}>{link}</Link>
+                ))}
+            </ul>
+
+            {/* Mobile menu starts here */}
+            <div className='flex justify-center items-center lg:hidden' onClick={toggleMenu}>
+                <div>
+                    {isMenuOpen ? <FaXmark className='text-white text-2xl cursor-pointer' /> : <FaBars className='text-white text-2xl cursor-pointer' />}
+                </div>
+            </div>
+            <div className={`${isMenuOpen ? 'flex' : 'hidden'} w-full h-fit bg-slate-800 p-4 absolute top-[80px] left-0`} >
+                <ul className='flex flex-col justify-center items-center gap-2 w-full'>
+                    {navItems.map(({ link, path}) => (
+                        <Link key={path} className='text-white uppercase font-semibold cursor-pointer p-3 rounded-lg hover:bg-yellow hover:text-black w-full text-center' to={path} spy={true} offset={-100} smooth={true}>{link}</Link>
+                    ))}
+                </ul>
+            </div>
+            <button className='bg-yellow px-8 py-3 rounded-full hover:bg-white hover:text-black font-bold mt-3 hidden lg:flex transform hover:scale-110 transition-transform duration-300 cursor-pointer'>BOOK NOW</button>
+        </nav>
     );
 }
 
